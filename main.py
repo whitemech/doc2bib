f46797978465dd5739404fa156ad86e0d7d0ec6f
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2021-05-25 15:39:35 +0200

    use bibtexparser

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 import re
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
@@ -15,16 +17,22 @@ drive = GoogleDrive(gauth)
 bibfile_doc = drive.CreateFile({'id': '19YR5EJ0-9s05lq4Utln8mrg2B6_LhJTudUwhJmoUvS4'})
 content = bibfile_doc.GetContentString("text/plain")
 
-pubs = re.search(r"%% NEW PAPERS ON TOP(.*)", content, flags=re.DOTALL)
+pubs = re.search(r"%% PUT BIB ENTRIES HERE ON TOP OF THIS LIST(.*)%% PUT BIB ENTRIES ON TOP OF THE LIST", content,
+                 flags=re.DOTALL)
 
-with open("tailor-wp5.bib", "w") as f:
-    f.write(pubs.group(1).strip())
+parser = BibTexParser(common_strings=True)
+parser.ignore_nonstandard_types = False
+parser.homogenise_fields = True
 
-os.remove("../whitemech.github.io/tailor-wp5.bib")
-shutil.move("tailor-wp5.bib", "../whitemech.github.io/")
+bib_db = bibtexparser.loads(pubs.group(1).strip(), parser=parser)
 
 r = Repo("../whitemech.github.io")
-r.git.checkout("master")
+r.git.checkout("tailor-pubs")
+r.git.rebase("master")
+
+with open('../whitemech.github.io/tailor-wp5.bib', 'w') as bibtex_file:
+    bibtexparser.dump(bib_db, bibtex_file)
+
 r.git.add(".")
 r.git.commit("-m", "automatic update tailor-wp5.bib")
 r.git.push()
